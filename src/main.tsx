f174f920fb19bc5c0ab7193b130ace04c1ce64cf
Trevor <trevorphilbrick@outlook.com>
2025-06-27 20:18:52 -0700

    turn on scroll restoration

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, scrollRestoration: true });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
