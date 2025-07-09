428b1aaeb5b27eb610e4236e48e5b61331571141
Trevor <trevorphilbrick@outlook.com>
2025-07-08 21:16:23 -0700

    reverted back to react target

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,10 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tanstackRouter({ target: "vercel" })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
+  ],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 });
